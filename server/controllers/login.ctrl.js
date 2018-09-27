868f10853b34f0dd2a84fc3b27a78313b1a28451
Ibrian Gomez Ortiz <ib.gomezo@gmail.com>
2018-09-27 16:52:00 -0300

    fix conflict of login controller

--- server/controllers/login.ctrl.js
+++ server/controllers/login.ctrl.js
@@ -3,35 +3,30 @@ const I_User = require('./../models').I_User
 module.exports = {
   /* Devuelve un formulario de login usado para testing */
   loginPage: (req, res, next) => {
-      res.send(`
-        <!DOCTYPE html>
-        <html>
-        <head>
-        <title>Page Title</title>
-        </head>Users
-        <body>
-          <form action="http://localhost:5000/login" method="post">
-            User name:<br>
-            <input type="text" name="username"><br>
-            User password:<br>
-            <input type="password" name="password">
-            <br><br>
-            <input type="submit" value="Submit">
-          </form> 
-        </body>
-        </html>
-      `
-      );
+    res.send(`
+      <!DOCTYPE html>
+      <html>
+      <head>
+      <title>Page Title</title>
+      </head>Users
+      <body>
+        <form action="http://localhost:5000/login" method="post">
+          User name:<br>
+          <input type="text" name="username"><br>
+          User password:<br>
+          <input type="password" name="password">
+          <br><br>
+          <input type="submit" value="Submit">
+        </form> 
+      </body>
+      </html>
+    `
+    );
   },
 
   loginFailed: (req, res, next) => {
-<<<<<<< 4040b6126468dca03fc97fe08aaa31833435ef6d
-    //res.json({ success: 'false', msg: 'El par usuario y contraseña es incorrecto' });
-    res.send('{ "success": "false", "msg": "El par usuario y contraseña es incorrecto" }');
-=======
-    res.json({ success: false, msg: 'El par usuario y contraseña es incorrecto' });
-    res.send();
->>>>>>> backend-frontend integration for Login action
+    let obj = { success: false, msg: "El par usuario y contraseña es incorrecto" };
+    res.send(obj);   
   },
 
   ensureLoggedIn: (req, res, next) => {
@@ -44,6 +39,7 @@ module.exports = {
 
   logout: (req, res, next) => {
     req.logout();
-    res.send('{ "success": "true", "msg": "Logout correcto" }');
+    let obj = { success: true, msg: "Logout correcto" };
+    res.send(obj);
   }
 }
